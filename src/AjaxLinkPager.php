f83e436025e58d4f0c1a50f6f2a7621846247eee
Aleksandr_Semenov <semenov@skeeks.com>
2018-05-03 10:22:07 +0300

    dev

---
+++ src/AjaxLinkPager.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @link https://cms.skeeks.com/
+ * @copyright Copyright (c) 2010 SkeekS
+ * @license https://cms.skeeks.com/license/
+ * @author Semenov Alexander <semenov@skeeks.com>
+ */
+
+namespace skeeks\yii2\ajaxpager;
+
+/**
+ * @author Semenov Alexander <semenov@skeeks.com>
+ */
+class AjaxLinkPager extends kop\y2sp\ScrollPager {
+    
+}
